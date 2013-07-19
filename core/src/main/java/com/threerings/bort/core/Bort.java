70dd7cf1e8d6cb9ab124f8a7550132dd42606383
David Hoover <karma@deadmoose.com>
2013-07-19 15:43:49 -0700

    TP it up

--- core/src/main/java/com/threerings/bort/core/Bort.java
+++ core/src/main/java/com/threerings/bort/core/Bort.java
@@ -1,31 +1,48 @@
 package com.threerings.bort.core;
 
-import static playn.core.PlayN.*;
-
 import playn.core.Game;
-import playn.core.Image;
-import playn.core.ImageLayer;
+import playn.core.PlayN;
+import playn.core.util.Clock;
+import tripleplay.game.ScreenStack;
+import tripleplay.game.UIScreen;
 
 public class Bort extends Game.Default {
+    /** Args from the Java bootstrap class. */
+    public static String[] mainArgs = {};
 
-  public Bort() {
-    super(33); // call update every 33ms (30 times per second)
-  }
+    public static final int UPDATE_RATE = 50;
 
-  @Override
-  public void init() {
-    // create and add background image layer
-    Image bgImage = assets().getImage("images/bg.png");
-    ImageLayer bgLayer = graphics().createImageLayer(bgImage);
-    graphics().rootLayer().add(bgLayer);
-  }
+    public Bort () {
+        super(UPDATE_RATE);
+    }
 
-  @Override
-  public void update(int delta) {
-  }
+    @Override public void init () {
+        _screens.push(new UIScreen() {
 
-  @Override
-  public void paint(float alpha) {
-    // the background automatically paints itself, so no need to do anything here!
-  }
+        });
+    }
+
+    @Override public void update (int delta) {
+        _clock.update(delta);
+        _screens.update(delta);
+    }
+
+    @Override public void paint (float alpha) {
+        _clock.paint(alpha);
+        _screens.paint(_clock);
+    }
+
+    protected final Clock.Source _clock = new Clock.Source(UPDATE_RATE);
+
+    protected final ScreenStack _screens = new ScreenStack() {
+        @Override protected void handleError (RuntimeException error) {
+            PlayN.log().warn("Screen failure", error);
+        }
+        @Override protected Transition defaultPushTransition () {
+            return slide();
+        }
+        @Override protected Transition defaultPopTransition () {
+            return slide().right();
+        }
+    };
 }
