27b19ce60d69126b5074f1806230a2a25784d1ab
David Hoover <karma@deadmoose.com>
2013-07-24 13:23:56 -0700

    Setup the TP platform bits

--- java/src/main/java/com/threerings/bort/java/BortJava.java
+++ java/src/main/java/com/threerings/bort/java/BortJava.java
@@ -3,14 +3,20 @@ package com.threerings.bort.java;
 import playn.core.PlayN;
 import playn.java.JavaPlatform;
 
+import tripleplay.platform.JavaTPPlatform;
+
 import com.threerings.bort.core.Bort;
 
 public class BortJava {
 
   public static void main(String[] args) {
     JavaPlatform.Config config = new JavaPlatform.Config();
+    config.width = 1024;
+    config.height = 768;
+
     // use config to customize the Java platform, if needed
-    JavaPlatform.register(config);
+    JavaPlatform jp = JavaPlatform.register(config);
+    JavaTPPlatform.register(jp, config);
     PlayN.run(new Bort());
   }
 }
