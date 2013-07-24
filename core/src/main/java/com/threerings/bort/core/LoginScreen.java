c1a127deeafacf600b3d78734da0c0dcc48019f3
David Hoover <karma@deadmoose.com>
2013-07-24 14:45:31 -0700

    Reduced it enough it doesn't break. Try adding in stylesheet?

--- core/src/main/java/com/threerings/bort/core/LoginScreen.java
+++ core/src/main/java/com/threerings/bort/core/LoginScreen.java
@@ -1,11 +1,19 @@
 package com.threerings.bort.core;
 
+import playn.core.Font;
 import playn.core.PlayN;
 import playn.core.util.Callback;
 import tripleplay.flump.Library;
+import tripleplay.ui.Field;
 import tripleplay.ui.Group;
 import tripleplay.ui.Icons;
 import tripleplay.ui.Label;
+import tripleplay.ui.SimpleStyles;
+import tripleplay.ui.Style;
+import tripleplay.ui.Style.TextEffect;
+import tripleplay.ui.Stylesheet;
+import tripleplay.ui.Tabs;
+import tripleplay.ui.Widget;
 import tripleplay.ui.layout.AxisLayout;
 
 /**
@@ -29,7 +37,6 @@ public class LoginScreen extends BortScreen
             public void onFailure (Throwable cause) {
                 PlayN.log().error("Couldn't load flump", cause);
             }
-
         });
     }
 
@@ -47,13 +54,29 @@ public class LoginScreen extends BortScreen
             return null;
         }
 
+        _root.setStylesheet(getBaseStylesheet());
+
         Group group = new Group(AxisLayout.vertical());
 
         group.add(new Label(Icons.image(_library.createTexture("Gut").layer().image())));
         group.add(new Label("Username:"));
 
-
         return group;
+    }
+
+    protected Stylesheet getBaseStylesheet ()
+    {
+        Font font = PlayN.graphics().createFont("Helvetica", Font.Style.BOLD, 20);
+
+        return SimpleStyles.newSheetBuilder().
+            add(Label.class, Style.HALIGN.left, Style.VALIGN.top).
+            add(Label.class, Style.COLOR.is(0xFFFFBE00)).
+            add(Widget.class, Style.FONT.is(font)).
+            add(Label.class, Style.TEXT_EFFECT.is(TextEffect.PIXEL_OUTLINE)).
+            add(Label.class, Style.HIGHLIGHT.is(0xFF000000)).
+            add(Tabs.class, Tabs.HIGHLIGHTER.is(Tabs.NOOP_HIGHLIGHTER)).
+            add(Field.class, Field.FULLTIME_NATIVE_FIELD.is(false)).
+            create();
     }
 
     protected Library _library;
